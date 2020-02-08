4db729b858a56efb133c29323024105153953243
zjcn86 <33260177+yikeshu0611@users.noreply.github.com>
2020-02-08 09:29:50 +0800

    add geom_sf_point to geom_sf
    Dear Professor
        I add a new geom_* "geom_sf_point" to geom_sf. So that people can add point to map easily to plot bubble map.
    Jing Zhang
    Respect

--- R/geom-sf.R
+++ R/geom-sf.R
@@ -234,6 +234,45 @@ geom_sf_label <- function(mapping = aes(), data = NULL,
     position <- position_nudge(nudge_x, nudge_y)
   }
 
+  
+#' @export
+#' @rdname ggsf
+#' @inheritParams stat_sf_coordinates
+geom_sf_point <-function(mapping = aes(), data = NULL,
+                         stat = "sf_coordinates", 
+                         position = "identity",
+                         nudge_x = NULL,
+                         nudge_y = NULL,
+                         na.rm = FALSE,
+                         show.legend = NA,
+                         inherit.aes = TRUE,
+                         fun.geometry = NULL,
+                         ...) {
+    if (!is.null(nudge_x) || !is.null(nudge_y)) {
+        if (!missing(position)) {
+            abort("You must specify either `position` or `nudge_x`/`nudge_y`.")
+        }
+        position <- position_nudge(nudge_x, nudge_y)
+    }
+    layer_sf(
+        data = data,
+        mapping = mapping,
+        stat = stat,
+        geom = GeomPoint,
+        position = position,
+        show.legend = show.legend,
+        inherit.aes = inherit.aes,
+        params = list(
+            na.rm = na.rm,
+            #it is import to give a value to shape
+            #so that geom_sf_point can be more easy used with
+            #scale_color/fille_grandientn/manual
+            fun.geometry = fun.geometry,
+            ...
+        )
+    )
+}
+
   layer_sf(
     data = data,
     mapping = mapping,
